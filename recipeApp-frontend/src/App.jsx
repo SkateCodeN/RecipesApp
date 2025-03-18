0e5dc4f0e345f46c593359b70119fe9c9ef53fcc
GR <immemo1105@yahoo.com>
2025-03-17 23:14:34 -0700

    ditto for routes in react router dom

--- recipeApp-frontend/src/App.jsx
+++ recipeApp-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import RecipesList from './Pages/RecipesList'
 import CreateRecipe from './Pages/CreateRecipe'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import ApiRecipe from './Pages/ApiRecipe'
 function App() {
 
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/recipesList" element={<RecipesList />} />
           <Route path="/createRecipe" element={<CreateRecipe />} />
+          <Route path='/apiRecipe' element={<ApiRecipe />} />
         </Routes>
       </BrowserRouter>
       
