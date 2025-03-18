9584903ddc8a90c7148262fb8df286b7082ccf7f
GR <immemo1105@yahoo.com>
2025-03-17 23:15:45 -0700

    created frontend test that receives remote recipe data

---
+++ recipeApp-frontend/src/Pages/ApiRecipe.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect} from "react";
+import "./RecipesList.css";
+
+const ApiRecipe = () => {
+    const [recipes, setRecipes] = useState({});
+    useEffect(() => {
+        getRecipes();
+    }, []);
+
+    const getRecipes = async () => {
+
+        try {
+            const response = await fetch("http://localhost:5234/api/recipes/test");
+            if (!response.ok) {
+                throw new Error("No connection to localhost:5234");
+            }
+            const data = await response.json();
+            setRecipes(data);
+            console.log("Data", data);
+        }
+        catch(error)
+        {
+            console.log('Error fetching recipes', error);
+        }
+
+    }
+
+    if(!recipes) return <p>Loading data</p>
+
+    return (
+        <div className="recipes-container">
+            <p>Testing</p>
+        </div>
+    )
+}
+
+export default ApiRecipe;
