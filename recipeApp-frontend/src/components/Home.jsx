006a06221144a37c48e0a7fa8de1ec63447b2fbc
GR <immemo1105@yahoo.com>
2025-03-22 23:12:36 -0700

    updated home UI

--- recipeApp-frontend/src/components/Home.jsx
+++ recipeApp-frontend/src/components/Home.jsx
@@ -1,7 +1,29 @@
 const Home = () => {
-    return(
+    return (
         <>
-            <h1>Recipes App</h1>
+            <div className="container">
+                <div className="container 
+                    is-max-tablet
+                    is-flex 
+                    
+                    is-justify-content-center
+                    is-align-content-center
+                ">
+                    <div className="card" style={{gap:"20px"}}>
+                        <h1 className="is-size-3">Recipe Generator</h1>
+
+                        <div className="is-flex" style={{ width:"400px" }}>
+                            <input
+                                className="input is-normal"
+                                type="text"
+                                placeholder="Enter Count"
+                            />
+                            <button className="button is-primary">Get Recipes</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
