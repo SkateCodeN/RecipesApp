3b535eda26628d92c12d4fd72231aba7ccd62b50
GR <immemo1105@yahoo.com>
2025-03-23 22:52:02 -0700

    navbar burger is toggling in smaller views

--- recipeApp-frontend/src/components/Navbar.jsx
+++ recipeApp-frontend/src/components/Navbar.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from 'react-router-dom';
 //import "./Navbar.css";
 
 const Navbar = () => {
+    const [isActive,setIsActive] = useState(false);
+
+    const handleToggle = () =>{
+        setIsActive( (prev) => !prev);
+    }
+
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation" >
             <div className="navbar-brand">
                 <Link to="/" className="navbar-item">
                     Home
                 </Link>
+
+                {/* Navbar button/ burger */}
+                <button 
+                    role="button"
+                    className={`navbar-burger ${isActive ? 'is-active' : ''}`}
+                    onClick={handleToggle}
+                    aria-label="menu"
+                    aria-expanded={isActive ? 'true' : 'false'}
+                    data-target='navbarMenu'
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </button>
+
             </div>
-            <div className="navbar-menu">
+            <div id="navbarMenu" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
                 <div className="navbar-end">
                         <Link to="/apiRecipe" className="navbar-item">
                             Test 
