37e0b45d4c2a6be8dd0defe60da5e800cf56ee75
GR <immemo1105@yahoo.com>
2025-03-23 23:38:29 -0700

    added icons and starting recipe list styling

--- recipeApp-frontend/src/components/Home.jsx
+++ recipeApp-frontend/src/components/Home.jsx
@@ -1,11 +1,12 @@
+
+
 const Home = () => {
     return (
-        <>
+        <div className="is-flex is-flex-direction-column">
             <div className="container">
                 <div className="container 
                     is-max-tablet
                     is-flex 
-                    
                     is-justify-content-center
                     is-align-content-center
                 ">
@@ -24,7 +25,40 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-        </>
+            <div className="container" style={{padding:"20px 0"}}>
+
+                {/* Here is the styling for each recipe, you can delete and replace */}
+                <div className="container is-max-tablet">
+                    <div className="card is-flex">
+                        <div style={{width:"60%",backgroundColor:"red"}}>
+                            <h2>Recipe Name</h2>
+                        </div>
+                        
+                        <div style={{width:"30%",backgroundColor:"blue"}}>
+                            <h3>Monday</h3>
+                        </div>
+                        
+                        <div className="is-flex" style={{gap:"10px"}}>
+                            <button 
+                                className="button is-danger" 
+                                type="button"
+                                title="delete recipe"
+                            >
+                                <img src='../../src/assets/delete.png' width={"20px"} />
+                            </button>
+                            <button 
+                                title="replace recipe"
+                                className="button is-warning" 
+                                type="button"
+                            >
+                                <img src='../../src/assets/replace.png' width={"20px"} />
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
