7776822f898f03508a69721db4bcdf232caa4ff8
GR <immemo1105@yahoo.com>
2025-03-26 22:05:11 -0700

    added route to the ai ui in router

--- recipeApp-frontend/src/App.jsx
+++ recipeApp-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import ApiRecipe from './Pages/ApiRecipe'
 import TastyAPI from "./Pages/TastyAPI";
+import AIRecipe from './Pages/AIRecipe'
 function App() {
 
   return (
@@ -19,6 +20,8 @@ function App() {
           <Route path="/createRecipe" element={<CreateRecipe />} />
           <Route path='/apiRecipe' element={<ApiRecipe />} />
           <Route path='/tasty' element={<TastyAPI />} />
+          
+          <Route path='/aiRecipes' element={<AIRecipe />} />
         </Routes>
       </BrowserRouter>
       
