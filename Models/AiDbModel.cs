7eb7ce4bf56d4a7abc9ec1ee969913754a9c41e6
GR <immemo1105@yahoo.com>
2025-03-29 23:08:18 -0700

    added more classes to the model as jsondata is a list of objects

--- Models/AiDbModel.cs
+++ Models/AiDbModel.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 namespace NoteApp.Models
 {
     [Table("airecipes")]
@@ -9,11 +11,45 @@ namespace NoteApp.Models
         public int Id {get; set;}
 
         [Column("jsondata")]
-        public string? JsonData {get; set;}
+        public List<JsonRecipes>? Data {get; set;}
 
         [Column("recipe_count")]
         public int? Recipe_Count {get; set;}
         
 
+    }
+
+   
+    public class JsonRecipes 
+    {
+        [JsonPropertyName("name")]
+        public string? Name {get; set;}
+
+        [JsonPropertyName("description")]
+        public string? Description {get; set;}
+
+        
+        [JsonPropertyName("ingredients")]
+        public List<Ingredient>? Ingredients {get; set;}
+
+        [JsonPropertyName("prep_time")]
+        public string? PrepTime {get; set;}
+
+        [JsonPropertyName("cook_time")]
+        public string? CookTime {get; set;}
+
+        [JsonPropertyName("instructions")]
+        public List<string>? Instructions {get; set;}
+    }
+    
+    public class Ingredient
+    {
+        [JsonPropertyName("ingredient")]
+        public string? RecipeIngredient {get; set;}
+
+        [JsonPropertyName("amount")]
+        public string? Amount {get; set;}
+
+        
     }
 }
