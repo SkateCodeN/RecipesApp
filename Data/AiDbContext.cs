9267239af136328ac1ce78d3d50a4749264bae38
GR <immemo1105@yahoo.com>
2025-03-29 23:04:12 -0700

    We creates on modelcreating to specifically serialize, match each property in jsondata and desirilize it according to the JsonRecipes class

--- Data/AiDbContext.cs
+++ Data/AiDbContext.cs
@@ -1,16 +1,37 @@
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using NoteApp.Models;
 
 namespace NoteApp.Data
 {
-    public class AiDbContext: DbContext
+    public class AiDbContext : DbContext
     {
         public AiDbContext(DbContextOptions<AiDbContext> options)
-            : base (options)
+            : base(options)
         {
 
         }
 
-        public DbSet<AiRecipe> AiRecipes {get; set;}
+        public DbSet<AiRecipe> AiRecipes { get; set; }
+
+        //In order to map each key in the jsondata object this override
+        //makes sure to match them 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            modelBuilder.Entity<AiRecipe>()
+                .Property(r => r.Data)
+                .HasConversion(
+                    v => JsonSerializer.Serialize(v, options),
+                    v => JsonSerializer.Deserialize<List<JsonRecipes>>(v, options)
+                )
+                .HasColumnName("jsondata");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
