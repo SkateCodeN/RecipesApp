de8592df6c1479a117d8c24811b03a1523da8844
GR <immemo1105@yahoo.com>
2025-04-01 22:00:44 -0700

    moved the Recipe component to its own component

--- recipeApp-frontend/src/components/Home.jsx
+++ recipeApp-frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 
-
+import Recipe from "./Recipe";
 const Home = () => {
     return (
         <div className="is-flex is-flex-direction-column">
@@ -30,33 +30,7 @@ const Home = () => {
                 {/* the recipe list will go here */}
                 <div className="container is-max-tablet">
                     {/* Here is the styling for each recipe, you can delete and replace */}
-                    <div className="card is-flex">
-                        <div style={{width:"60%",backgroundColor:"red"}}>
-                            <h2>Recipe Name</h2>
-                        </div>
-                        
-                        <div style={{width:"30%",backgroundColor:"blue"}}>
-                            <h3>Monday</h3>
-                        </div>
-                        
-                        <div className="is-flex" style={{gap:"10px"}}>
-                            <button 
-                                className="button is-danger" 
-                                type="button"
-                                title="delete recipe"
-                            >
-                                <img src='../../src/assets/delete.png' width={"20px"} />
-                            </button>
-                            <button 
-                                title="replace recipe"
-                                className="button is-warning" 
-                                type="button"
-                            >
-                                <img src='../../src/assets/replace.png' width={"20px"} />
-                            </button>
-                        </div>
-
-                    </div>
+                    <Recipe />
                 </div>
             </div>
         </div>
