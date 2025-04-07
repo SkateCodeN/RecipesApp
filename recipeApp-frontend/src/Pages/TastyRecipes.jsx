95508e0c7a993c84c051d3548e4e4ba6a1d77330
GR <immemo1105@yahoo.com>
2025-04-06 21:28:24 -0700

    laying the foundation of what to do cause I aint doing notin today

---
+++ recipeApp-frontend/src/Pages/TastyRecipes.jsx
@@ -0,0 +1,3 @@
+//Todo a form component and a recipes component
+// The form takes in a param to send to the backend
+// The tasty recipes component receives a JSon an outputs recipes 
---
+++ recipeApp-frontend/src/components/RecipeCards.js
@@ -0,0 +1,2 @@
+// This can take in a Json for those tasty recipe API and output a
+// card list of those recipes
