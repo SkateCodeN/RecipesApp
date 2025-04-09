1f81aa4b4c240a8493da8b0591cc62df24d61888
GR <immemo1105@yahoo.com>
2025-04-09 01:37:44 -0700

    added comments to recipe list that will actually be our suggestions

--- recipeApp-frontend/src/components/RecipeList.jsx
+++ recipeApp-frontend/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import Recipe from "./Recipe";
 
+//Home Page Displays random recipe suggestions
 const RecipeList =({count}) =>{
     
     const [recipeList, setRecipeList] = useState([]);
